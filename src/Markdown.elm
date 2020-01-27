e3336f8153fb7befa037efee0f6d1716b7b1c04a
Rupert Smith <rupert@thesett.com>
2020-01-27 15:53:16 +0000

    Starting point for a styled renderer.

--- src/Markdown.elm
+++ src/Markdown.elm
@@ -1,11 +1,11 @@
 module Markdown exposing (..)
 
 import Html
-import Html.Attributes
+import Html.Attributes as Attr
 import Html.Styled exposing (Html, div, form, h1, h4, img, label, p, pre, span, styled, text, toUnstyled)
 import Markdown.Block exposing (Block)
 import Markdown.Html
-import Markdown.Parser
+import Markdown.Parser exposing (Renderer)
 import Metadata exposing (Metadata)
 import Pages.Document
 
@@ -41,5 +41,85 @@ markdownRenderer =
     { default
         | image =
             \{ src } alt ->
-                Html.img [ Html.Attributes.src src, Html.Attributes.style "width" "100%" ] [ Html.text alt ] |> Ok
+                Html.img [ Attr.src src, Attr.style "width" "100%" ] [ Html.text alt ] |> Ok
+    }
+
+
+defaultHtmlRenderer : Renderer (Html.Html msg)
+defaultHtmlRenderer =
+    { heading =
+        \{ level, children } ->
+            case level of
+                1 ->
+                    Html.h1 [] children
+
+                2 ->
+                    Html.h2 [] children
+
+                3 ->
+                    Html.h3 [] children
+
+                4 ->
+                    Html.h4 [] children
+
+                5 ->
+                    Html.h5 [] children
+
+                6 ->
+                    Html.h6 [] children
+
+                _ ->
+                    Html.text "TODO maye use a type here to clean it up... this will never happen"
+    , raw = Html.p []
+    , bold =
+        \content -> Html.strong [] [ Html.text content ]
+    , italic =
+        \content -> Html.em [] [ Html.text content ]
+    , code =
+        \content -> Html.code [] [ Html.text content ]
+    , link =
+        \link content ->
+            Html.a [ Attr.href link.destination ] content
+                |> Ok
+    , image =
+        \image content ->
+            Html.img [ Attr.src image.src ] [ Html.text content ]
+                |> Ok
+    , plain =
+        Html.text
+    , unorderedList =
+        \items ->
+            Html.ul []
+                (items
+                    |> List.map
+                        (\itemBlocks ->
+                            Html.li []
+                                itemBlocks
+                        )
+                )
+    , orderedList =
+        \startingIndex items ->
+            Html.ol
+                (if startingIndex /= 1 then
+                    [ Attr.start startingIndex ]
+
+                 else
+                    []
+                )
+                (items
+                    |> List.map
+                        (\itemBlocks ->
+                            Html.li []
+                                itemBlocks
+                        )
+                )
+    , html = Markdown.Html.oneOf []
+    , codeBlock =
+        \{ body, language } ->
+            Html.pre []
+                [ Html.code []
+                    [ Html.text body
+                    ]
+                ]
+    , thematicBreak = Html.hr [] []
     }
