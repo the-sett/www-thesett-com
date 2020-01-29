4c9106e12b23b96d85b725d9079213f1c45ada8a
Rupert Smith <rupert@thesett.com>
2020-01-29 11:45:10 +0000

    Fixed issue with static site not rendering.

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// import hljs from "highlight.js/lib/highlight";
-// import "highlight.js/styles/github.css";
-// import elm from 'highlight.js/lib/languages/elm';
-// hljs.registerLanguage('elm', elm);
-// window.hljs = hljs;
-//setTimeout(function() {hljs.initHighlighting();}, 50);
+import "./style.css";
 
 const { Elm } = require("./src/Main.elm");
 const pagesInit = require("elm-pages");
